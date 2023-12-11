2648aa2a6f787de7d607b9d7ecda21c7586c016f
andreas tsimerikas <andreastsimerikas@gmail.com>
2023-12-12 00:14:05 +0200

    Update modeling_deformable_detr.py

--- src/transformers/models/deformable_detr/modeling_deformable_detr.py
+++ src/transformers/models/deformable_detr/modeling_deformable_detr.py
@@ -2070,9 +2070,9 @@ def sigmoid_focal_loss(inputs, targets, salience, num_boxes, alpha: float = 0.25
     Returns:
         Loss tensor
     """
-    # print(f"targets: {targets}")
-    # print(f"inputs: {inputs}")
-    # print(f"salience: {salience}")
+    print(f"targets: {targets}")
+    print(f"inputs: {inputs}")
+    print(f"salience: {salience}")
     
     prob = inputs.sigmoid()
     ce_loss = nn.functional.binary_cross_entropy_with_logits(inputs, targets, reduction="none")
@@ -2080,7 +2080,7 @@ def sigmoid_focal_loss(inputs, targets, salience, num_boxes, alpha: float = 0.25
     # add modulating factor
     p_t = prob * targets + (1 - prob) * (1 - targets)
     loss = ce_loss * ((1 - p_t) ** gamma)
-    
+    print(f"lossbefore: {loss}")
     if alpha >= 0:
         alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
         print(f"alpha_t: {alpha_t}")
