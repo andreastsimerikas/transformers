f337cc3865cc7d05fd00b80fad036758865de93f
andreas tsimerikas <andreastsimerikas@gmail.com>
2023-12-22 14:18:51 +0200

    Update modeling_deformable_detr.py

--- src/transformers/models/deformable_detr/modeling_deformable_detr.py
+++ src/transformers/models/deformable_detr/modeling_deformable_detr.py
@@ -2087,7 +2087,7 @@ def sigmoid_focal_loss(inputs, targets, salience, num_boxes, alpha: float = 0.25
     if alpha >= 0:
         alpha_t = alpha * targets + (1 - alpha) * (1 - targets)
         # print(f"alpha_t: {alpha_t}")
-        loss = alpha_t * (salience+(a_ss-1) * loss
+        loss = alpha_t * (salience+(a_ss-1)) * loss
     # print(f"salience_loss: {salience_loss}")
     return salience_loss.mean(1).sum() / num_boxes
 
